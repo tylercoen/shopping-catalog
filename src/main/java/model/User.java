a17f56c33948ad66ccb5d5724dc6608e98ba8998
tyler <tyler@tylerspc.mynetworksettings.com>
2026-03-02 19:02:37 -0500

    Create user class, still need to add overrides

---
+++ src/main/java/model/User.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	// default constructor
+
+	public User() {
+
+	}
+
+	// parameterized constructor
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
+
+}
